d3da7bd8a2243cd885769f69d7af0e16ecebc44d
Colin Wageman <colin@sosolimited.com>
2014-09-04 14:16:53 -0700

    twit and socket.io added to the application, currently using Alex's twitter access

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+var dataTwitterGetResult_1;		// Results from 1st Twitter GET query.
+
+//This is the front end
+$(function(){
+
+	console.log('connecting to socket');
+	//attempt to require paper
+
+	// connect to the socket server
+	var socket = io();
+
+	// if we get an "info" emit from the socket server then console.log the data we recive
+	socket.on('info', function (data) {
+	    console.log(data);
+	});
+	
+	// Receive Twitter REST Search results.
+	socket.on('eTwitterGetResult_1', function (data) {
+		dataTwitterGetResult_1 = data;
+		console.log(data);
+		// Print data to webpage just to see it.
+		printTwitterResults(dataTwitterGetResult_1, '#twitter_results_1 ul');
+
+	});
+
+});
+
+// Print data from Twitter JS object into an html element.
+function printTwitterResults(data, domElementTitle) {
+	$.each(data, function(key, value){
+		var tweetText = value.text;
+		console.log("print Tweets");
+		$(domElementTitle).append('<li>'+ tweetText.cleanTweet() +'</li>');
+	});
+}
+
+// Common function, to clean a tweet of unwanted features.
+String.prototype.cleanTweet = function() {		
+	var tweet = this;
+	// NB: we're not doing it now, but it's possible to store the regex matches in arrays for later use. We would need to use .
+	tweet = tweet.replace(/@([a-zA-Z0-9]+)/g, "");	// remove user mentions.
+	tweet = tweet.replace(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g, "");	// remove http links.
+	tweet = tweet.replace(/: /, " ");		// remove colons at the end of a word.
+	$.trim(tweet);	// Remove leading and trailing whitespace.
+		// return this.replace(/^\s+|\s+$/g,"");		// regex for trim fct.
+
+	return tweet;
+}
--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
+var http			 = require('http');
+var server = http.createServer(app);
+var logger		 = require('morgan');
+var Twit 			 = require('twit');
+var io				 = require('socket.io').listen(server);
 
 // configure app
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,10 +19,62 @@ var port     = process.env.PORT || 8080; // set our port
 
 var apiRouting    = require('./routes/api');
 
+app.use(logger());
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res){
+  res.send('Hello');
+});
+
 // REGISTER OUR ROUTES -------------------------------
 app.use('/api', apiRouting);
 
+// Open a socket to stream results continuously to our webpage.
+io.on('connection', function (socket) {
+  console.log('A new user connected!');
+
+	var twitterQuery_1 = "BarackObama";
+	var stream_phrase = "football";
+
+	console.log("Using Alex's keys");
+	 //Alex's keys
+	 var T = new Twit({
+	     consumer_key: 'P8EYI0gloJoDTOu8596QcUn1c'
+	   , consumer_secret: 'Ya8PmkQxm7FLdQ6coftOi65hSedUNevFVil0kApw45YEI22mMd'
+	   , access_token: '234878749-OlktDQaRgvr6hkBoQ4kI94y7sxI1EfpOlH17rwTG'
+	   , access_token_secret: 'nKpgBcCd20RFXeASCLwACtA80PnEmvBJ6kJcaeA4oSO4a'
+	});
+
+
+	// Twitter API usage - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// Do REST search.
+	T.get('statuses/user_timeline', { screen_name: twitterQuery_1, count: 10 }, function(err, data, response) {
+	    if (err) {
+	      console.log("ERROR- app.js- search #1.");
+	      console.error(err.stack);
+	    }
+	    //console.log(data);
+	    console.log("tweets pulled");
+	    socket.emit('eTwitterGetResult_1', data);
+	});
+
+	// --- STREAM OFF FOR NOW ---
+  // // Everytime there's a new tweet, emit a event passing the tweet.
+  // var stream = T.stream('statuses/filter', { track: stream_phrase });
+  // stream.on('tweet', function (tweet) {
+  //   socket.emit('twitter_stream', tweet);
+  // });
+
+});
+
+// --- STREAM OFF FOR NOW ---
+// // Everytime there's a new tweet, emit a event passing the tweet.
+// var stream = T.stream('statuses/filter', { track: stream_phrase });
+// stream.on('tweet', function (tweet) {
+//   socket.emit('twitter_stream', tweet);
+// });
+
 // START THE SERVER
 // =============================================================================
-app.listen(port);
+//app.listen(port);
+server.listen(port);
 console.log('Magic happens on port ' + port);
